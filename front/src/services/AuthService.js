ae2555b2e42c72feb3c1fa14cba17f12435d2933
Hxzxrd <hxzxrd@vk.com>
2023-05-19 16:48:28 +0300

    create password recovery

--- front/src/services/AuthService.js
+++ front/src/services/AuthService.js
@@ -1,31 +1,39 @@
 import $api from "../http";
-import React from 'react';
+import React from "react";
 
 const AuthService = () => {
-    const login = async (email, password) => {
-        return $api.post('/login', {email, password})
-    }
+  const login = async (email, password) => {
+    return $api.post("/login", { email, password });
+  };
 
-    const registration = async (
-        login,
-        password,
-        fio,
-        email
-) => {
-        return $api.post('/registration', {login, password, fio, email})
-    }
+  const registration = async (login, password, fio, email) => {
+    return $api.post("/registration", { login, password, fio, email });
+  };
 
-    const logout = async () => {
-        return $api.post('/logout')
-    }
+  const logout = async () => {
+    return $api.post("/logout");
+  };
 
-     const setPassword = async (id, password) => {
-        return $api.post(`/set-password/${id}`, {password})
-     }
+  const setPassword = async (id, password) => {
+    return $api.post(`/set-password/${id}`, { password });
+  };
 
+  const recoveryCode = async (email) => {
+    return $api.post(`/recovery-code/`, { email });
+  };
 
+  const recoveryPassword = async (code, password) => {
+    return await $api.post(`/recovery/`, { code, password });
+  };
 
-    return {login, registration, logout, setPassword}
-}
+  return {
+    login,
+    registration,
+    logout,
+    setPassword,
+    recoveryCode,
+    recoveryPassword,
+  };
+};
 
-export default AuthService
+export default AuthService;
